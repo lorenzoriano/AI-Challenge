5cb6a6099663db6e072e453c65d4d3e7dcd55a32
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-11-14 21:51:45 +0000

    removed unused file

--- profile.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-export BROWSER=/usr/bin/firefox
-tools/playgame.py -E --turntime=1000000000 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 152 --map_file tools/maps/maze/maze_04p_01.map  "python MyBot.py" "python tools/sample_bots/python/LeftyBot.py" "python tools/sample_bots/python/HunterBot.py" "python tools/sample_bots/python/GreedyBot.py" --fill --verbose
