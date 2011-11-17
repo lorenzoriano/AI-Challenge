a99d0839e2debcf0d0d52a7a705d6cb635f8cb41
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-11-17 01:20:37 +0000

    added multiple maps

--- play_one_game_2vs2.sh
+++ play_one_game_2vs2.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 export BROWSER=/usr/bin/firefox
-tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500 --map_file tools/maps/random_walk/random_walk_02p_02.map  "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose
+rm current_bot/bot.txt
+#tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/maze/maze_02p_01.map
+#tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/maze/maze_02p_02.map
+#tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_02p_02.map
+#tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_04p_02.map
+tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_03p_02.map
