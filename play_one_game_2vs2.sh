53b36c4fcb62cf510ac2e81fc2e566da117dd29f
Lorenzo Riano <lorenzo.riano@gmail.com>
2011-12-17 02:43:40 +0000

    new options

--- play_one_game_2vs2.sh
+++ play_one_game_2vs2.sh
@@ -2,8 +2,10 @@
 export BROWSER=/usr/bin/firefox
 rm -f current_bot/bot.txt
 #tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/maze/maze_02p_01.map
-tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000 "python current_bot/MyBot.py" "python oldbot2/oldbot2.py" --fill --verbose --map_file tools/maps/random_walk/random_walk_02p_02.map
+#tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000 "python current_bot/MyBot.py" "python oldbot3/oldbot3.py" --fill --verbose --map_file tools/maps/random_walk/random_walk_02p_02.map
 #tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/maze/maze_02p_02.map
 #tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_02p_02.map
 #tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 500   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_04p_02.map
 #tools/playgame.py -E --turntime=200 -O --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000   "python current_bot/MyBot.py"  "python oldbot1/oldbot1.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_03p_02.map
+
+tools/playgame.py -E --turntime=400 -O --end_wait=0.25 --log_dir game_logs --turns 1000   "python current_bot/MyBot.py"  "python oldbot3/oldbot3.py"  --fill --verbose --map_file tools/maps/random_walk/random_walk_03p_02.map
